b8c843cd8c5c50805c9b24b5231662ba168edbfe
FlorizV <fvogelzangdump@hotmail.com>
2023-01-26 12:12:04 +0100

    t/m add main index page

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n)+!te36eqwu&*8#qbi0p*m6v)jkaqs#8_=svgfq#1si9lk$(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com']
 
 
 # Application definition
